56c192d18ea813a982f35ec009c56ed4d3d81fa0
Damien HERBERT <dherbert98@gmail.com>
2020-04-09 15:09:42 +0200

    modif

--- modele.h
+++ modele.h
@@ -6,29 +6,29 @@
 #include <netinet/in.h>
 
 typedef struct {
-    uint64_t id;
-    uint16_t seqno;
-    char* data;
+  uint64_t id;
+  uint16_t seqno;
+  char data[];
 } donnee;
 
 typedef struct {
-    struct sin6_addr* ip;
-    in_port_t port;
+  struct sin6_addr* ip;
+  struct sin6_port* port;
 } socket;
 
 typedef struct {
-    socket s;
-    char permanent;
-    time_t last_change;
+  socket s;
+  char permanent;
+  time_t last_change;
 } voisin;
 
 typedef struct {
-    uint8_t type;
-    size_t length;
-    socket s;
-    uint16_t network_hash;
-    uint16_t node_hash;
-    donnee data;
+  uint8_t type;
+  size_t length;
+  socket s;
+  uint16_t network_hash;
+  uint16_t node_hash;
+  donnee data;
 } tlv;
 
 int sum(uint16_t seqno, int n);
